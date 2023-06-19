becef1948b8a7c990f3e34895467c594af2fce3f
Can Kolho <can.kolho@hotmail.com>
2023-06-19 23:03:14 +0300

    Website animations

--- js/alert.js
+++ js/alert.js
@@ -1,5 +1,5 @@
 window.onload = function() {
   setTimeout(function() {
     alert("Privacy Notice:\n\nBy submitting form below, you acknowledge and agree that your name and email address will be collected solely for the purpose of responding to your message. I use a trusted third-party service called FormSubmit.co to securely process and store your information. I value your privacy and assure you that your information will not be shared with any other third parties. Thank you!");
-  }, 1000);
+  }, 2800);
 }
